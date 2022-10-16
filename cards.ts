158bfd4385b4e387f52232dd0427ac36b4a350a3
Chris Evans <chris@ceva24.dev>
2022-10-16 17:16:57 +0100

    fix: generate correct image url for ability card data

--- cards.ts
+++ cards.ts
@@ -38,7 +38,7 @@ const createAbilityCardEntriesForClass = (classAbbreviation: string): AbilityCar
             id: parseInt(abilityCardData.cardno),
             name: titleCase(abilityCardData.name),
             level: abilityCardData.level,
-            imageUrl: abilityCardData.image.replace(".png", ".webp")
+            imageUrl: `/images/${abilityCardData.image.replace(".png", ".webp")}`
         }
     });
 
