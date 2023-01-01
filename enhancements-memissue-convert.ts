8c60004e92daf2b7cbd9944895c0c25c804e78f5
Chris Evans <chris@ceva24.dev>
2023-01-01 23:03:23 +0000

    chore: add brute trample card enhancement data and temp scripts

---
+++ enhancements-memissue-convert.ts
@@ -0,0 +1,40 @@
+import { readdirSync } from "node:fs";
+import Jimp from "jimp";
+import { CWebp } from "cwebp";
+
+const location = './images/character-ability-cards-enhanced/gloomhaven/BR/gh-trample/';
+
+const convert = async () => {
+
+    const files = readdirSync(location);
+
+    const filesToConvert = files.slice(7000);
+
+    for (let i = 0; i < filesToConvert.length; i++) {
+        const fileLocation = location.concat(filesToConvert[i]);
+        const baseFileLocation = fileLocation.substring(0, fileLocation.lastIndexOf(".")).replace("gh-trample", "gh-trample-webqual");
+
+        console.log(baseFileLocation);
+
+        const imageData = await Jimp.read(fileLocation);
+        createJpgImage(imageData, baseFileLocation);
+        await createWebpImage(fileLocation, baseFileLocation);
+    }
+}
+
+const createJpgImage = async (imageData: Jimp, imagePath: string) => {
+    const jpgImagePath = imagePath.concat(".jpg");
+
+    imageData.resize(300, 400).quality(65).write(jpgImagePath);
+}
+
+const createWebpImage = async (pngPath: string, imagePath: string) => {
+    const webpImagePath = imagePath.concat(".webp");
+
+    const encoder = new CWebp(pngPath);
+    encoder.resize(300, 400);
+
+    await encoder.write(webpImagePath);
+}
+
+convert();
