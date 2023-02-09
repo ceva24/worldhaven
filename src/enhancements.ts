7086682501905b182c5528cc58b2717b803cb5a0
Chris Evans <chris@ceva24.dev>
2023-02-09 20:28:53 +0000

    feat: add MT level 5-9 enhancement data

--- src/enhancements.ts
+++ src/enhancements.ts
@@ -19,7 +19,7 @@ interface ChosenEnhancement {
 }
 
 const main = async () => {
-    const card = cards[16];
+    const card = cards[25];
     const cardImage = await Jimp.read(`./${card.imageUrl}`);
 
     const enhancementIconImages: Map<string, Jimp> = new Map(await Promise.all(enhancements.filter((enhc: Enhancement) => enhc.imageUrl).map(async (enhc: Enhancement) => ([enhc.name, await Jimp.read(`./${enhc.imageUrl}`)] as [string, Jimp]))));
