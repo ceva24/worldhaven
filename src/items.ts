516dbfbc40b7a8d2b7b877a1df69de8b677ea3d9
Chris Evans <chris@ceva24.dev>
2023-03-25 02:07:20 +0000

    feat: add script to generate item data

---
+++ src/items.ts
@@ -0,0 +1,51 @@
+import { readFileSync } from "fs";
+import startCase from "lodash.startcase";
+
+interface ItemData {
+    name: string;
+    points: number;
+    expansion: string;
+    image: string;
+    xws: string;
+}
+
+interface Item {
+    id: number;
+    name: string;
+    imageUrl: string;
+    slot: string;
+    slotImageUrl: string;
+    group: string;
+    alternativeImageUrl: string;
+}
+
+const data = readFileSync("./data/items.js");
+const itemData: ItemData[] = JSON.parse(data.toString()) as ItemData[]
+
+const createItemEntries = () => {
+    const entries = createItemEntriesForLevel("36-42");
+
+    console.log(JSON.stringify(entries, null, 4));
+}
+
+const createItemEntriesForLevel = (level: string): Item[] => {
+    const gameItems = itemData.filter((item: ItemData) => {
+        return item.expansion === "Gloomhaven" && !item.name.startsWith("item") && item.image.match(level) && !item.image.match("-back.png")
+    });
+
+    const items: Item[] = gameItems.map((item: ItemData) => {
+        return {
+            id: 22,
+            name: startCase(item.name),
+            imageUrl: item.image.replace(".png", ".webp"),
+            slot: "Head",
+            slotImageUrl: "/equip-slot-icons/gloomhaven/head.webp",
+            group: "5",
+            alternativeImageUrl: "",
+        }
+    });
+
+    return items;
+}
+
+createItemEntries();
