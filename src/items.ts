5da92b4015e23a4f464e33d09730da2e8f9bfb7c
Chris Evans <chris@ceva24.dev>
2023-03-25 19:20:52 +0000

    feat: update items script

--- src/items.ts
+++ src/items.ts
@@ -23,24 +23,24 @@ const data = readFileSync("./data/items.js");
 const itemData: ItemData[] = JSON.parse(data.toString()) as ItemData[]
 
 const createItemEntries = () => {
-    const entries = createItemEntriesForLevel("36-42");
+    const entries = createItemEntriesForLevel("64-151", "9");
 
     console.log(JSON.stringify(entries, null, 4));
 }
 
-const createItemEntriesForLevel = (level: string): Item[] => {
+const createItemEntriesForLevel = (level: string, group: string): Item[] => {
     const gameItems = itemData.filter((item: ItemData) => {
         return item.expansion === "Gloomhaven" && !item.name.startsWith("item") && item.image.match(level) && !item.image.match("-back.png")
     });
 
     const items: Item[] = gameItems.map((item: ItemData) => {
         return {
-            id: 22,
+            id: 50,
             name: startCase(item.name),
             imageUrl: item.image.replace(".png", ".webp"),
             slot: "Head",
             slotImageUrl: "/equip-slot-icons/gloomhaven/head.webp",
-            group: "5",
+            group,
             alternativeImageUrl: "",
         }
     });
